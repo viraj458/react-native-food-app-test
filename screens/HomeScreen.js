5a8c281b7ac696add19b067e0aa0a7dbbce03f5a
Viraj <virajmalaka94@gmail.com>
2022-10-27 19:42:27 +0530

    create search area

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { ChevronDownIcon, UserIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import { ChevronDownIcon, UserIcon, MagnifyingGlassIcon, AdjustmentsVerticalIcon } from "react-native-heroicons/outline";
 
 
 
@@ -17,7 +17,7 @@ const HomeScreen = () => {
   return (
     <SafeAreaView className='bg-white pt-5'>
       {/*Header*/}
-      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+      <View className="flex-row py-3 items-center mx-4 space-x-2">
         <View>
           <Image
             source={{
@@ -29,12 +29,21 @@ const HomeScreen = () => {
         <View className='flex-1'>
           <Text className='font-bold text-gray-500 text-xs'>Deliver Now</Text>
           <Text className='font-bold text-xl'>Current Location
-          <ChevronDownIcon color="00CCBB" size={20} />
+          <ChevronDownIcon color="#00CCBB" size={20} />
           </Text>
         </View>
         <View>
-          <UserIcon size={35} color="00CCBB"/>
+          <UserIcon size={30} color='#00CCBB'/>
         </View>
+      </View>
+
+      {/*Search*/}
+      <View className='flex-row mx-4 space-x-3 items-center mb-2'>
+        <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
+          <MagnifyingGlassIcon size={20} color='#00CCBB'/>
+          <TextInput placeholder='Restaurants and cuisines'/>
+        </View>
+        <AdjustmentsVerticalIcon color='#00CCBB'/>
       </View>
     </SafeAreaView>
   )
